072b46769a6eff11f163070cdc313ee2eac9b211
Balakrishnan N <60445196+bkrish1719205@users.noreply.github.com>
2020-06-30 14:52:56 +0530

    Add files via upload

--- mrbpage.py
+++ mrbpage.py
@@ -6,15 +6,7 @@ app = Flask(__name__)
 def mrb_home():
     return render_template('homepg.html')
 
-@app.route('/contacts')
-def mrb_contact():
-    return render_template('contactas.html')
-@app.route('/blog')
-def mrb_blog():
-    return render_template('blog.html')
-@app.route('/websites')
-def mrb_website():
-    return render_template('websites.html')
+
 
 if __name__ == "__main__" :
     app.run()
